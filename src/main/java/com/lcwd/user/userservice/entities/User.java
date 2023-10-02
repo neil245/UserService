802e73ebaa6dd28b81043bed01b9da247d97f0c6
Xellos@166 <wdibangshu@gmail.com>
2023-10-03 00:45:43 +0200

    Correction made and docker compose file included for future use

--- src/main/java/com/lcwd/user/userservice/entities/User.java
+++ src/main/java/com/lcwd/user/userservice/entities/User.java
@@ -1,6 +1,9 @@
 package com.lcwd.user.userservice.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,7 +18,7 @@ import lombok.Setter;
 
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    //@GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "ID")
     private String userId;
     @Column(name = "NAME")
